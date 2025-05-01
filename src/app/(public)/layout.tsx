86bb4dcc15574875d4f3b9a9f410e32de4ed5b63
Hector Miranda <marcetux@gmail.com>
2025-05-01 12:47:19 -0700

    fixing routes

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { ReactNode } from 'react';
+
 export default function PublicLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <div className="min-h-screen bg-[#0B1120]">
