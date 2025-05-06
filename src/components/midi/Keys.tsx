9a54810818fca6bf2263684c6c5f2f402faa3419
Hector Miranda <marcetux@gmail.com>
2025-05-05 19:39:22 -0700

    keys re mapping

--- src/components/midi/Keys.tsx
+++ src/components/midi/Keys.tsx
@@ -17,7 +17,23 @@ export function Keys({ activeKeys, onKeyPress }: KeysProps) {
 
   // Create black keys
   const blackKeyPattern = [1, 1, 0, 1, 1, 1, 0]; // 1 for black key, 0 for gap
-  const blackKeyPositions = whiteKeyPositions.filter((_, i) => blackKeyPattern[i % 7]);
+
+  // Map white keys to MIDI notes (starting from C2 = 36)
+  const whiteKeyToMidi = (index: number) => 36 + index;
+
+  // Create array of positions for black keys
+  const blackKeyPositions = [];
+  let blackKeyIndex = 0;
+  
+  for (let i = 0; i < whiteKeyPositions.length; i++) {
+    if (blackKeyPattern[i % 7]) {
+      blackKeyPositions.push({
+        position: whiteKeyPositions[i],
+        midiNote: whiteKeyToMidi(i) + 1, // The black key is 1 semitone above its white key
+        index: blackKeyIndex++
+      });
+    }
+  }
 
   return (
     <group>
@@ -26,29 +42,29 @@ export function Keys({ activeKeys, onKeyPress }: KeysProps) {
         <mesh
           key={`white-${i}`}
           position={[x - (whiteKeyPositions.length * whiteKeyWidth) / 2, 0, 0]}
-          onClick={() => onKeyPress(36 + i)}
+          onClick={() => onKeyPress(whiteKeyToMidi(i))}
         >
           <boxGeometry args={[whiteKeyWidth * 0.95, 0.1, whiteKeyLength]} />
           <meshStandardMaterial 
-            color={activeKeys.has(36 + i) ? '#4a9eff' : 'white'} 
+            color={activeKeys.has(whiteKeyToMidi(i)) ? '#4a9eff' : 'white'} 
           />
         </mesh>
       ))}
 
       {/* Black keys */}
-      {blackKeyPositions.map((x, i) => (
+      {blackKeyPositions.map((keyInfo) => (
         <mesh
-          key={`black-${i}`}
+          key={`black-${keyInfo.index}`}
           position={[
-            x + (whiteKeyWidth / 2) - (whiteKeyPositions.length * whiteKeyWidth) / 2,
+            keyInfo.position + (whiteKeyWidth / 2) - (whiteKeyPositions.length * whiteKeyWidth) / 2,
             0.05,
             -(whiteKeyLength - blackKeyLength) / 2
           ]}
-          onClick={() => onKeyPress(37 + i)}
+          onClick={() => onKeyPress(keyInfo.midiNote)}
         >
           <boxGeometry args={[blackKeyWidth, 0.1, blackKeyLength]} />
           <meshStandardMaterial 
-            color={activeKeys.has(37 + i) ? '#4a9eff' : 'black'} 
+            color={activeKeys.has(keyInfo.midiNote) ? '#4a9eff' : 'black'} 
           />
         </mesh>
       ))}
--- src/components/midi/ThreeScene.tsx
+++ src/components/midi/ThreeScene.tsx
@@ -16,8 +16,24 @@ function Keys({ activeKeys, onKeyPress }: ThreeSceneProps) {
   const blackKeyLength = 0.7;
 
   const whiteKeyPositions = Array.from({ length: 52 }, (_, i) => i * whiteKeyWidth);
-  const blackKeyPattern = [1, 1, 0, 1, 1, 1, 0];
-  const blackKeyPositions = whiteKeyPositions.filter((_, i) => blackKeyPattern[i % 7]);
+  const blackKeyPattern = [1, 1, 0, 1, 1, 1, 0]; // 1 for black key, 0 for gap
+  
+  // Map white keys to MIDI notes (starting from C2 = 36)
+  const whiteKeyToMidi = (index: number) => 36 + index;
+
+  // Create array of positions for black keys
+  const blackKeyPositions = [];
+  let blackKeyIndex = 0;
+  
+  for (let i = 0; i < whiteKeyPositions.length; i++) {
+    if (blackKeyPattern[i % 7]) {
+      blackKeyPositions.push({
+        position: whiteKeyPositions[i],
+        midiNote: whiteKeyToMidi(i) + 1, // The black key is 1 semitone above its white key
+        index: blackKeyIndex++
+      });
+    }
+  }
 
   return (
     <group>
@@ -25,25 +41,25 @@ function Keys({ activeKeys, onKeyPress }: ThreeSceneProps) {
         <mesh
           key={`white-${i}`}
           position={[x - (whiteKeyPositions.length * whiteKeyWidth) / 2, 0, 0]}
-          onClick={() => onKeyPress(36 + i)}
+          onClick={() => onKeyPress(whiteKeyToMidi(i))}
         >
           <boxGeometry args={[whiteKeyWidth * 0.95, 0.1, whiteKeyLength]} />
-          <meshStandardMaterial color={activeKeys.has(36 + i) ? '#4a9eff' : 'white'} />
+          <meshStandardMaterial color={activeKeys.has(whiteKeyToMidi(i)) ? '#4a9eff' : 'white'} />
         </mesh>
       ))}
 
-      {blackKeyPositions.map((x, i) => (
+      {blackKeyPositions.map((keyInfo) => (
         <mesh
-          key={`black-${i}`}
+          key={`black-${keyInfo.index}`}
           position={[
-            x + (whiteKeyWidth / 2) - (whiteKeyPositions.length * whiteKeyWidth) / 2,
+            keyInfo.position + (whiteKeyWidth / 2) - (whiteKeyPositions.length * whiteKeyWidth) / 2,
             0.05,
             -(whiteKeyLength - blackKeyLength) / 2
           ]}
-          onClick={() => onKeyPress(37 + i)}
+          onClick={() => onKeyPress(keyInfo.midiNote)}
         >
           <boxGeometry args={[blackKeyWidth, 0.1, blackKeyLength]} />
-          <meshStandardMaterial color={activeKeys.has(37 + i) ? '#4a9eff' : 'black'} />
+          <meshStandardMaterial color={activeKeys.has(keyInfo.midiNote) ? '#4a9eff' : 'black'} />
         </mesh>
       ))}
     </group>
